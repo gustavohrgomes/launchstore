99e70f39229d6dc4d44582945fe17852f00b709e
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-03-16 22:23:05 -0300

    Create products register form

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const routes = express.Router();
+const ProductsController = require("./app/controllers/ProductsController");
 
 routes.get("/", (req, res) => {
   return res.render("layout.njk");
+});
+
+// PRODUCTS
+routes.get("/products/create", ProductsController.create);
+
+routes.get("/ads/create", (req, res) => {
+  return res.redirect("/products/create");
 });
 
 module.exports = routes;
