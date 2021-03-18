0efd3262b81349c4e251ece4904f43c319613ac3
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-03-17 21:58:02 -0300

    Minor Changes

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 
 module.exports = new Pool({
   user: "postgres",
-  password: "launchstore",
+  password: "1234",
   host: "localhost",
   port: 5432,
   database: "launchstore",
