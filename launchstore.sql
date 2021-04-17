71852b6a1194cc155d8fda6c93d57c7f8deb58c4
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-04-17 18:15:51 -0300

    Update launchstore.sql

--- launchstore.sql
+++ launchstore.sql
@@ -1,3 +1,6 @@
+DROP DATABASE IF EXISTS "launchstore";
+CREATE DATABASE "launchstore";
+
 CREATE TABLE "products" (
   "id" SERIAL PRIMARY KEY,
   "category_id" int,
@@ -24,13 +27,31 @@ CREATE TABLE "files" (
   "product_id" int
 );
 
-ALTER TABLE "products" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text NOT NULL,
+  "email" text UNIQUE NOT NULL,
+  "password" text NOT NULL,
+  "cpf_cnpj" int UNIQUE NOT NULL,
+  "cep" text,
+  "address" text,
+  "created_at" timestamp DEFAULT (NOW()),
+  "updated_at" timestamp DEFAULT (NOW())
+)
 
+-- FOREIGN KEYS
+ALTER TABLE "products" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
+ALTER TABLE "products" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 ALTER TABLE "files" ADD FOREIGN KEY ("product_id") REFERENCES "products" ("id");
+
+-- INSERTS
+INSERT INTO "categories" (name) VALUES ('comida');
+INSERT INTO "categories" (name) VALUES ('eletronicos');
+INSERT INTO "categories" (name) VALUES ('informática');
 
 /* TRIGGERS & PROCEDURES */
 CREATE FUNCTION trigger_set_timestamp()
-RETURNS TRIGGER $$	
+RETURNS TRIGGER AS $$	
 BEGIN
 	NEW.updated_at = NOW();
   RETURN NEW;
@@ -39,5 +60,10 @@ $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER set_timestamp
 BEFORE UPDATE ON products
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON users
 FOR EACH ROW
 EXECUTE PROCEDURE trigger_set_timestamp();
