38ec1e5b843fb68fb07f6f2769eab575b5a57545
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-04-17 20:37:59 -0300

    Separation of routes in different files

---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const routes = express.Router();
+
+const HomeController = require("../app/controllers/HomeController");
+
+const users = require("./users");
+const products = require("./products");
+
+routes.use("/products", products);
+routes.use("/users", users);
+
+// HOME
+routes.get("/", HomeController.index);
+
+// Alias
+routes.get("/ads/create", (req, res) => {
+  return res.redirect("/products/create");
+});
+
+routes.get("/accounts", (req, res) => {
+  return res.redirect("/users/register");
+});
+
+module.exports = routes;
---
+++ src/routes/users.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const routes = express.Router();
+
+const SessionController = require("../app/controllers/SessionController");
+const UserController = require("../app/controllers/UserController");
+
+// // Login/Logout
+// routes.get("/users/login", SessionController.loginForm);
+// routes.post("/users/login", SessionController.login);
+// routes.post("/users/logout", SessionController.logout);
+
+// // Reset/Forgot Password
+// routes.get("/forgot-password", SessionController.forgotForm);
+// routes.get("/password-reset", SessionController.resetForm);
+// routes.post("/forgot-password", SessionController.forgot);
+// routes.post("/password-reset", SessionController.reset);
+
+// // User Register
+routes.get("/register", UserController.registerForm);
+// routes.post("/register", UserController.post);
+
+// routes.get("/", UserControllers.show);
+// routes.put("/", UserControllers.put);
+// routes.delete("/", UserControllers.delete);
+
+module.exports = routes;
