8a3587dd4abd93007c46ca71a902b35ebbde78e2
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-04-27 08:36:25 -0300

    Update user validator

--- src/app/validators/user.js
+++ src/app/validators/user.js
@@ -1,13 +1,33 @@
 const User = require("../models/User");
+const { compare } = require("bcryptjs");
 
-async function post(req, res, next) {
-  // check if has all fields
-  const keys = Object.keys(req.body);
+function checkAllFields(body) {
+  const keys = Object.keys(body);
 
   for (value of keys) {
     if (req.body[value] == "") {
-      return res.render("user/register", { user: req.body, error: "Por favor, preencha todos os campos." });
+      return { user: body, error: "Por favor, preencha todos os campos." };
     }
+  }
+}
+
+async function show(req, res, next) {
+  const { userId: id } = req.session;
+
+  const user = await User.findOne({ where: { id } });
+
+  if (!user) return res.render("user/register", { error: "Usuário não encontrado" });
+
+  req.user = user;
+
+  next();
+}
+
+async function post(req, res, next) {
+  // check if has all fields
+  const fillAllFields = checkAllFields(req.body);
+  if (fillAllFields) {
+    return res.render("user/register", fillAllFields);
   }
 
   // check if user exists [email, cpf_cnpj]
@@ -30,6 +50,32 @@ async function post(req, res, next) {
   next();
 }
 
+async function update(req, res, next) {
+  // all fields
+  const fillAllFields = checkAllFields(req.body);
+  if (fillAllFields) {
+    return res.render("user/register", fillAllFields);
+  }
+
+  // has password
+  const { id, password } = req.body;
+
+  if (!password) return res.render("user/index", { user, error: "Digite sua senha para atualizar seu cadastro." });
+
+  // password match
+  const user = await User.findOne({ where: { id } });
+
+  const passed = await compare(password, user.password);
+
+  if (!passed) return res.render("user/index", { user, error: "Senha incorreta." });
+
+  req.user = user;
+
+  next();
+}
+
 module.exports = {
+  show,
   post,
+  update,
 };
