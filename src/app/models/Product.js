23b964e280696de596b7438b8ad25ffa7629b317
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-05-24 21:58:00 -0300

    Adjust in search product

--- src/app/models/Product.js
+++ src/app/models/Product.js
@@ -11,33 +11,25 @@ module.exports = {
 
     return results.rows;
   },
-  async search(params) {
-    const { filter, category } = params;
-
-    let query = "";
-    filterQuery = `WHERE`;
-
-    if (category) {
-      filterQuery = `
-        ${filterQuery}
-        products.category_id = ${category}
-        AND 
-      `;
-    }
-
-    filterQuery = `
-      ${filterQuery}
-      products.name ILIKE '%${filter}%'
-      OR products.description ILIKE '%${filter}%'
-    `;
-
-    query = `
+  async search({ filter, category }) {
+    let query = `
       SELECT products.*, categories.name as category_name
       FROM products
       LEFT JOIN categories ON (categories.id = products.category_id)
-      ${filterQuery}
-      GROUP BY products.id, categories.name
+      WHERE 1 = 1
     `;
+
+    if (category) {
+      query += `AND products.category_id = ${category} \n`;
+    }
+
+    if (filter) {
+      query += `AND (products.name ILIKE '%${filter}%' OR products.description ILIKE '%${filter}%') \n`;
+    }
+
+    query += `AND status != 0`;
+
+    console.log(query);
 
     const results = await db.query(query);
     return results.rows;
